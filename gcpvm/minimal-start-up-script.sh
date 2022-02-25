1a53cb3615e3cf0d67b7ec40655a966e6e8d4bce
Ryan Avery <ravery@ucsb.edu>
2022-02-24 17:37:26 -0800

    fix syncdown and persist mounting between make stop and start but only for directories created before terraform apply

--- gcpvm/minimal-start-up-script.sh
+++ gcpvm/minimal-start-up-script.sh
@@ -3,3 +3,4 @@ rm cuda-repo-ubuntu2004-11-3-local_11.3.0-465.19.01-1_amd64.deb # forgot to do t
 echo "Connection with GCS Fuse. accessing a path will dynamically mount it. any path to a bucket subdir can be accessed if the bucket is in the project"
 mkdir -p data/
 gcsfuse --implicit-dirs data/
+printf "cerulean /root/data gcsfuse rw,x-systemd.requires=network-online.target,user\n" >> /etc/fstab #persists mounting
