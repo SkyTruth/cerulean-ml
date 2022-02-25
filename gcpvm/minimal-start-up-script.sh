086b01cf9a68d3bf37e87ed2b03b9aef213c2288
Ryan Avery <ravery@ucsb.edu>
2022-02-25 10:26:47 -0800

    add aliases for mounting and starting jupyter

--- gcpvm/minimal-start-up-script.sh
+++ gcpvm/minimal-start-up-script.sh
@@ -2,5 +2,6 @@ rm cuda-repo-ubuntu2004-11-3-local_11.3.0-465.19.01-1_amd64.deb # forgot to do t
 ### Mounting
 echo "Connection with GCS Fuse. accessing a path will dynamically mount it. any path to a bucket subdir can be accessed if the bucket is in the project"
 mkdir -p data/
-gcsfuse --implicit-dirs data/
-printf "cerulean /root/data gcsfuse rw,x-systemd.requires=network-online.target,user\n" >> /etc/fstab #persists mounting
+gcsfuse --implicit-dirs cerulean data/
+printf "alias cdata='gcsfuse --implicit-dirs cerulean data/'\n" >> /root/.bashrc #persists mounting
+printf "alias jserve='jupyter lab --allow-root --no-browser'\n" >> /root/.bashrc #start jupyter
