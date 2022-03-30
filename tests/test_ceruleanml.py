865f531759388d0ae1564b627b77d5bf48e0488a
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-03-30 14:38:42 +0200

    Add minimal run tests

--- tests/test_ceruleanml.py
+++ tests/test_ceruleanml.py
@@ -7,6 +7,7 @@ import pytest
 from click.testing import CliRunner
 
 from ceruleanml import ceruleanml
+from ceruleanml import data
 from ceruleanml import cli
 
 
@@ -35,3 +36,46 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_dist_array_from_tile():
+    layer_path = ["tests/fixtures/S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5/cv2_transfer_outputs_skytruth_annotation_first_phase_old_vessel_S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5_ambiguous_1.png"]
+    data.COCOtiler.dist_array_from_tile(layer_path)
+
+
+def test_create_coco_from_photopea_layers():
+    info = {
+        "description": "Cerulean Dataset V2",
+        "url": "none",
+        "version": "1.0",
+        "year": 2021,
+        "contributor": "Skytruth",
+        "date_created": "2022/2/23"
+    }
+
+    licenses = [
+        {
+            "url": "none",
+            "id": 1,
+            "name": "CeruleanDataset V2"
+        }
+    ]
+    categories = [{"supercategory": "slick", "id": 1, "name": "infra_slick"},
+                  {"supercategory": "slick", "id": 2, "name": "natural_seep"},
+                  {"supercategory": "slick", "id": 3, "name": "coincident_vessel"},
+                  {"supercategory": "slick", "id": 4, "name": "recent_vessel"},
+                  {"supercategory": "slick", "id": 5, "name": "old_vessel"},
+                  {"supercategory": "slick", "id": 6, "name": "ambiguous"}]
+
+    coco_output = {
+        "info": info,
+        "licenses": licenses,
+        "images": [],
+        "annotations": [],
+        "categories": categories
+    }
+    coco_tiler = data.COCOtiler("tiled_images/", coco_output)
+    layer_path = ["tests/fixtures/S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5/cv2_transfer_outputs_skytruth_annotation_first_phase_old_vessel_S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5_Background.png",
+                  "tests/fixtures/S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5/cv2_transfer_outputs_skytruth_annotation_first_phase_old_vessel_S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5_ambiguous_1.png"]
+
+    coco_tiler.save_background_img_tiles(layer_path, aux_datasets=["/Users/rodrigoalmeida/cerulean-ml/inverted.json"])
