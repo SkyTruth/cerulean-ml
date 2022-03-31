93f9f67be9026ca67a034bc70b748335d16b1988
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-03-31 09:49:57 +0200

    Format

--- tests/test_ceruleanml.py
+++ tests/test_ceruleanml.py
@@ -37,21 +37,34 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'ceruleanml.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    assert "ceruleanml.cli.main" in result.output
+    help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert "--help  Show this message and exit." in help_result.output
 
 
 def mock_scene_info():
-    return {"mocked": True, 'bounds': [55.698181, 24.565813, 58.540211, 26.494711], 'band_metadata': [['vv', {}], ['vh', {}]], 'band_descriptions': [['vv', ''], ['vh', '']], 'dtype': 'uint16', 'nodata_type': 'Nodata', 'colorinterp': ['gray', 'gray']}
+    return {
+        "mocked": True,
+        "bounds": [55.698181, 24.565813, 58.540211, 26.494711],
+        "band_metadata": [["vv", {}], ["vh", {}]],
+        "band_descriptions": [["vv", ""], ["vh", ""]],
+        "dtype": "uint16",
+        "nodata_type": "Nodata",
+        "colorinterp": ["gray", "gray"],
+    }
 
 
 def test_dist_array_from_tile(httpx_mock):
     httpx_mock.add_response(json=mock_scene_info())
-    layer_path = ["tests/fixtures/S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5/cv2_transfer_outputs_skytruth_annotation_first_phase_old_vessel_S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5_ambiguous_1.png"]
+    layer_path = [
+        "tests/fixtures/S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5/cv2_transfer_outputs_skytruth_annotation_first_phase_old_vessel_S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5_ambiguous_1.png"
+    ]
     arr = data.COCOtiler.dist_array_from_tile(
-        layer_path, vector_ds="tests/fixtures/oil_areas_inverted_clip.geojson", resample_ratio=10)
+        layer_path,
+        vector_ds="tests/fixtures/oil_areas_inverted_clip.geojson",
+        resample_ratio=10,
+    )
     assert arr.shape == (4181, 6458)
     assert arr.dtype == np.uint8
 
@@ -64,29 +77,25 @@ def test_create_coco_from_photopea_layers(httpx_mock):
         "version": "1.0",
         "year": 2021,
         "contributor": "Skytruth",
-        "date_created": "2022/2/23"
+        "date_created": "2022/2/23",
     }
 
-    licenses = [
-        {
-            "url": "none",
-            "id": 1,
-            "name": "CeruleanDataset V2"
-        }
+    licenses = [{"url": "none", "id": 1, "name": "CeruleanDataset V2"}]
+    categories = [
+        {"supercategory": "slick", "id": 1, "name": "infra_slick"},
+        {"supercategory": "slick", "id": 2, "name": "natural_seep"},
+        {"supercategory": "slick", "id": 3, "name": "coincident_vessel"},
+        {"supercategory": "slick", "id": 4, "name": "recent_vessel"},
+        {"supercategory": "slick", "id": 5, "name": "old_vessel"},
+        {"supercategory": "slick", "id": 6, "name": "ambiguous"},
     ]
-    categories = [{"supercategory": "slick", "id": 1, "name": "infra_slick"},
-                  {"supercategory": "slick", "id": 2, "name": "natural_seep"},
-                  {"supercategory": "slick", "id": 3, "name": "coincident_vessel"},
-                  {"supercategory": "slick", "id": 4, "name": "recent_vessel"},
-                  {"supercategory": "slick", "id": 5, "name": "old_vessel"},
-                  {"supercategory": "slick", "id": 6, "name": "ambiguous"}]
 
     coco_output = {
         "info": info,
         "licenses": licenses,
         "images": [],
         "annotations": [],
-        "categories": categories
+        "categories": categories,
     }
 
     with tempfile.TemporaryDirectory() as tmp_dir:
@@ -95,12 +104,18 @@ def test_create_coco_from_photopea_layers(httpx_mock):
         class_file = "tests/fixtures/S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5/cv2_transfer_outputs_skytruth_annotation_first_phase_old_vessel_S1A_IW_GRDH_1SDV_20200802T141646_20200802T141711_033729_03E8C7_E4F5_ambiguous_1.png"
         template = skio.imread(class_file)
         background_file = class_file.replace("ambiguous_1", "Background")
-        skio.imsave(background_file, template[:,:,0])
+        skio.imsave(background_file, template[:, :, 0])
         layer_path = [background_file, class_file]
 
         # Pass same vector dataset twice to make RGB image
         coco_tiler.save_background_img_tiles(
-            layer_path, aux_datasets=["tests/fixtures/oil_areas_inverted_clip.geojson", "tests/fixtures/oil_areas_inverted_clip.geojson"], resample_ratio=10)
-        
+            layer_path,
+            aux_datasets=[
+                "tests/fixtures/oil_areas_inverted_clip.geojson",
+                "tests/fixtures/oil_areas_inverted_clip.geojson",
+            ],
+            resample_ratio=10,
+        )
+
         os.remove(background_file)
         assert len(os.listdir(tmp_dir)) == 117
