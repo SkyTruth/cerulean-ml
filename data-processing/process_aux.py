662b6a4b7d8ba22593b13c37b343b98655ccb120
Rodrigo Almeida <rodrigo@developmentseed.org>
2022-04-01 15:57:11 +0200

    Add stub for process ship density

--- data-processing/process_aux.py
+++ data-processing/process_aux.py
@@ -1,5 +1,6 @@
 import geopandas as gpd 
 from shapely.geometry import box
+import rasterio
 
 def invert_geom(ds="location_of_the_worlds_petroleum_fields__xtl.json", out="inverted.json"):
     whole_world = gpd.GeoDataFrame(index=[0], geometry=[box(-180.0000, -90.0000, 180.0000, 90.0000)], crs=4326)
@@ -16,3 +17,8 @@ def process_csv_infra(ds="ml_aux_files_detections_2020_infrastructureOnly_v20210
     df_nongeo = gpd.read_file(ds)
     df_geo = gpd.GeoDataFrame(df_nongeo, geometry=gpd.points_from_xy(df.Lon, df.Lat))
     df_geo.to_file(out)
+
+def process_tif_ship_density(f="ShipDensity_Commercial1.tif", out="ship_density.json"):
+    with rasterio.open(f) as src:
+        density = src.read(0)
+        
