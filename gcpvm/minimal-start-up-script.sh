017e042ac7bc525f1d78d8bae41cd3c5b33de903
Jona <jona.raphael@gmail.com>
2024-08-08 16:08:25 -0400

    mistake in the startup script code

--- gcpvm/minimal-start-up-script.sh
+++ gcpvm/minimal-start-up-script.sh
@@ -5,8 +5,8 @@ mkdir -p data/
 mkdir -p data-cv2/
 gcsfuse --implicit-dirs ceruleanml data/
 gcsfuse --implicit-dirs cv2-training data-cv2/
-printf "alias cdata='gcsfuse --implicit-dirs /root/ceruleanml-biggpuregion data/'\n" >> /root/.bashrc #persists mounting of processed data bucket
-printf "alias cdata2='gcsfuse --implicit-dirs /root/cv2-training data-cv2/'\n" >> /root/.bashrc #persists mounting of source data bucket. treat as read only!
+printf "alias cdata='gcsfuse --implicit-dirs ceruleanml-biggpuregion /root/data/'\n" >> /root/.bashrc #persists mounting of processed data bucket
+printf "alias cdata2='gcsfuse --implicit-dirs cv2-training /root/data-cv2/'\n" >> /root/.bashrc #persists mounting of source data bucket. treat as read only!
 printf "alias jserve='jupyter lab --allow-root --no-browser'\n" >> /root/.bashrc #start jupyter
 printf "export CUDA_HOME=/usr/local/cuda"
 printf "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
