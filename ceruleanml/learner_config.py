328d860b4f5f04e556258cd2d3a7698e8b8cd7f7
Ethan Bevan <74314722+ancientpi3@users.noreply.github.com>
2024-08-12 22:31:23 -0400

    such confusion

--- ceruleanml/learner_config.py
+++ ceruleanml/learner_config.py
@@ -226,7 +226,6 @@ def get_tfms(
                 max_scale=rrc_crop_area_proportion,
                 ratio=(1, 1),
             ),
-            RandomErasing(max_count=3),
         ]
         
         if augs.get('do_random_erasing', False):
